e916a39c3abd932f6f37cf971318fc761dd124a8
aftiyah <abd2010ly@gmail.com>
2020-04-24 23:57:26 +0200

    Add _DATA and API

---
+++ src/utils/API.js
@@ -0,0 +1,34 @@
+import { AsyncStorage } from "react-native";
+import Data from "./_DATA";
+
+export const DECKS_STORAGE_KEY = "mobileFlashcards:Decks";
+
+export function initialData() {
+  AsyncStorage.setItem(DECKS_STORAGE_KEY, JSON.stringify(Data));
+  return data;
+}
+
+export function fetchDecks() {
+  return AsyncStorage.getItem(DECKS_STORAGE_KEY).then(results => {
+    return results === null ? initialData() : JSON.parse(results);
+  });
+}
+
+export function createDeck(deck) {
+  return AsyncStorage.mergeItem(DECKS_STORAGE_KEY, JSON.stringify(deck));
+}
+
+export function addQuestion({ card, deckName }) {
+  return AsyncStorage.getItem(DECKS_STORAGE_KEY, (err, result) => {
+    let decks = JSON.parse(result);
+
+    let newQuestions = JSON.parse(JSON.stringify(decks[deckName].questions));
+    newQuestions[newQuestions.length] = card;
+
+    const value = JSON.stringify({
+      [deckName]: { title: deckName, questions: newQuestions }
+    });
+
+    AsyncStorage.mergeItem(DECKS_STORAGE_KEY, value);
+  });
+}
---
+++ src/utils/_DATA.js
@@ -0,0 +1,24 @@
+export default data = {
+    Proramming: {
+      title: "Proramming",
+      questions: [
+        {
+          question: "What is your favorite programming language?",
+          answer: "Javascript"
+        },
+        {
+          question: "What is the best Javascript library?",
+          answer: "React"
+        }
+      ]
+    },
+    Sports: {
+      title: "Sports",
+      questions: [
+        {
+          question: "What sport do you play the most ?",
+          answer: "Football"
+        }
+      ]
+    }
+  };
