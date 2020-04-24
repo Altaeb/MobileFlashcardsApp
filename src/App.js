51cc1316786474de903cddeabdafc8c631a79d8c
aftiyah <abd2010ly@gmail.com>
2020-04-25 00:43:16 +0200

    Add Buttom nav tab

--- src/App.js
+++ src/App.js
@@ -5,13 +5,37 @@ import AddDeck from "./components/AddDeck";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducer from "./reducers";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 export default function App() {
+  const Tab = createBottomTabNavigator();
   return (
     <Provider store={createStore(reducer)}>
-      <View style={styles.container}>
-        <AddDeck />
-      </View>
+    <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+
+              if (route.name === "Dec kList") {
+                iconName = "ios-pricetags";
+              } else {
+                iconName = "ios-add";
+              }
+              return <Ionicons name={iconName} />;
+            }
+          })}
+          tabBarOptions={{
+            activeTintColor: "blue",
+            inactiveTintColor: "gray"
+          }}
+        >
+          <Tab.Screen name="Dec kList" component={DeckList} />
+          <Tab.Screen name="Add Deck" component={AddDeck} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 }
